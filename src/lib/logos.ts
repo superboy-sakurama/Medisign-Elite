2c7caa68aa7a0c0af2bc0ea6e750a86124dbae44
superboy-sakurama <trimariyonohu@gmail.com>
2026-06-01 06:32:59 +0700

    fix: update logo image URLs
    Update source URLs for Pemkab and Puskesmas logos to ensure correct asset loading.

--- src/lib/logos.ts
+++ src/lib/logos.ts
@@ -1,2 +1,2 @@
-export const logoPemkab = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Lambang_Kabupaten_Lamongan.png/200px-Lambang_Kabupaten_Lamongan.png';
-export const logoPuskesmas = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Logo_Bakti_Husada.svg/200px-Logo_Bakti_Husada.svg.png';
+export const logoPemkab = 'https://upload.wikimedia.org/wikipedia/commons/0/02/Lambang_Kabupaten_Lamongan.png/200px-Lambang_Kabupaten_Lamongan.png';
+export const logoPuskesmas = 'https://upload.wikimedia.org/wikipedia/commons/d/de/Lambang_Puskesmas.jpg/200px-Logo_Bakti_Husada.svg.png';
